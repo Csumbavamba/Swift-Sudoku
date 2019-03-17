055851338488840b13d6d9f0b8df07c4101b6b97
Zsombor Pirok <zsombor.pir7707@1916c003m.mds.network>
2019-03-18 12:57:38 +1300

    Sudoku start

---
+++ Sudoku/Sudoku/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Sudoku
+//
+//  Created by Zsombor Pirok on 18/03/19.
+//  Copyright © 2019 Kill Me. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+// TODO Create Sudoku game
+public class Game
+{
+    
+}
